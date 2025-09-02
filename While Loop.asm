a99b34bcb79b2420feaee55bebd2b181d46ba0db
Md.Shahriar Imtiaz <115032515+shahriarimtiazofficial@users.noreply.github.com>
2025-09-02 15:00:29 +0600

    Add files via upload

---
+++ While Loop.asm
@@ -0,0 +1,49 @@
+.model small
+.stack 100h
+
+.data
+    msg db 'Enter input: $'   
+    msg2 db 13, 10, '$'                      
+.code
+main proc
+    
+    mov ax, @data
+    mov ds, ax
+
+    lea dx, msg                     
+    mov ah, 9                
+    int 21h
+
+    mov dx, 0                        
+    mov ah, 1                       
+    int 21h                         
+
+while_loop:
+    cmp al, 0dh                     
+    je endl                         
+    inc dx                       
+    int 21h                          
+    jmp while_loop                 
+
+endl:
+    mov bl, dl                     
+    mov ah,9                      
+    int 21h
+    add bl, 30h                      
+
+
+    mov dl, bl                    
+    mov ah, 02h                   
+    int 21h                       
+    
+   
+    lea dx, msg2
+    mov ah, 9
+    int 21h
+
+ 
+    mov ah, 4Ch                       
+    int 21h                          
+
+main endp
+end main
